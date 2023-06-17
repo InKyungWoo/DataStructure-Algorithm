f25ffe5a4ea033f06bee613cd141068e6df98ec9
Inkyung <chelsea1201@naver.com>
2023-06-18 06:26:04 +0900

    주석 추가

--- Algorithm/goorm_실습/heap.py
+++ Algorithm/goorm_실습/heap.py
@@ -36,3 +36,11 @@ L = [int(x) for x in input().split()]
 H = Heap(L)
 H.heap_sort()
 print(H)
+
+'''
+- heapify_down(self, k, n) : 인덱스 k에서 시작하여 재귀적으로 작동하며, 자식 노드 중 값이 더 큰 노드와 부모 노드를 교환하는 과정을 반복하여 하향식으로 힙을 정렬한다.
+
+- make_heap(self) : 배열의 중간부터 시작하여 역순으로 heapify_down 함수를 호출함으로써 배열 전체를 힙으로 변환한다.
+
+- heap_sort(self) : 힙의 루트 노드(가장 큰 값)와 마지막 노드를 교환한 후, 마지막 노드를 제외하고 힙을 다시 정렬하는 과정을 반복한다.
+'''
