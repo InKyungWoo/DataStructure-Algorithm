8e0f9d1dfcb517e7ee16b2c54895f62e62e24606
Inkyung <chelsea1201@naver.com>
2023-06-17 22:59:49 +0900

    04 Recursion: 재귀reverse

---
+++ Algorithm/goorm_실습/05_재귀reverse.py
@@ -0,0 +1,9 @@
+def reverse(L, a):
+	n = len(L)
+	if a < n//2 :
+		L[a], L[n-1-a] = L[n-1-a], L[a]
+		reverse(L, a+1)   # 재귀 호출
+
+A = list(input()) # 문자열을 입력받아 리스트로 변환
+reverse(A)
+print(''.join(str(x) for x in A))
